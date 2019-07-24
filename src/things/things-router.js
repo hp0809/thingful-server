733c7715ace87cea3117a1d7a2b80f9e88ccb8bb
Hali Power <hpower0809@gmail.com>
2019-07-24 22:06:47 +0000

    secure login checkpoint

--- src/things/things-router.js
+++ src/things/things-router.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const ThingsService = require('./things-service')
-const {requireAuth} = require('../middleware/basic-auth')
+const { requireAuth } = require('../middleware/jwt-auth')
 
 const thingsRouter = express.Router()
 
@@ -36,7 +36,6 @@ thingsRouter.route('/:thing_id/reviews/')
       .catch(next)
   })
 
-/* async/await syntax for promises */
 async function checkThingExists(req, res, next) {
   try {
     const thing = await ThingsService.getById(
--- test/protected-endpoints-spec.js
+++ test/protected-endpoints-spec.js
@@ -36,43 +36,36 @@ describe(`Protected endpoints`, function () {
     const protectedEndpoints = [
         {
           name: 'GET /api/things/:thing_id',
-          path: '/api/things/1'
+          path: '/api/things/1',
+          method: supertest(app).get,
         },
         {
           name: 'GET /api/things/:thing_id/reviews',
-          path: '/api/things/1/reviews'
+          path: '/api/things/1/reviews',
+          method: supertest(app).post,
         },
     ]
         
     protectedEndpoints.forEach(endpoint => {
         describe(endpoint.name, () => {  
-            it(`responds with 401 'Missing basic token' when no basic token`, () => {
-              return supertest(app)
-                .get(endpoint.path)
-                .expect(401, { error: `Missing basic token` })
-            })
+            it(`responds 401 'Missing bearer token' when no bearer token`, () => {
+                return endpoint.method(endpoint.path)
+                  .expect(401, { error: `Missing bearer token` })
+              })
   
-            it(`responds 401 'Unauthorized request' when no credentials in token`, () => {
-              const userNoCreds = { user_name: '', password: '' }
-                return supertest(app)
-                  .get(endpoint.path)
-                  .set('Authorization', helpers.makeAuthHeader(userNoCreds))
+              it(`responds 401 'Unauthorized request' when invalid JWT secret`, () => {
+                const validUser = testUsers[0]
+                const invalidSecret = 'bad-secret'
+                return endpoint.method(endpoint.path)
+                  .set('Authorization', helpers.makeAuthHeader(validUser, invalidSecret))
                   .expect(401, { error: `Unauthorized request` })
-            })
-            it(`responds 401 'Unauthorized request' when invalid user`, () => {
-              const userInvalidCreds = { user_name: 'user-not', password: 'existy' }
-                return supertest(app)
-                  .get(endpoint.path)
-                  .set('Authorization', helpers.makeAuthHeader(userInvalidCreds))
+              })
+              it(`responds 401 'Unauthorized request' when invalid sub in payload`, () => {
+                const invalidUser = { user_name: 'user-not-existy', id: 1 }
+                return endpoint.method(endpoint.path)
+                  .set('Authorization', helpers.makeAuthHeader(invalidUser))
                   .expect(401, { error: `Unauthorized request` })
-            })
-            it(`responds 401 'Unauthorized request' when invalid password`, () => {
-              const userInvalidPass = { user_name: testUsers[0].user_name, password: 'wrong' }
-                return supertest(app)
-                  .get(endpoint.path)
-                  .set('Authorization', helpers.makeAuthHeader(userInvalidPass))
-                  .expect(401, { error: `Unauthorized request` })
-            })
+              })
           })
         })
     
